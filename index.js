78c121fd0faeb4141e3ce57acf52f249b1677ba5
RandomCubers <66269330+RandomCubers@users.noreply.github.com>
2020-06-03 11:47:12 -0700

    Moderatoin UPDATE :)

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Discord = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
 const client = new Discord.Client();
 
+var prefix = 'mb!' // Prefix for bot
+var bot_prefix = ['!', '-','<@']; //Bot Prefixes
 
 client.on('ready', () =>{
     console.log('This bot is online!') 
 
-    client.user.setStatus('idle') // Set bot to idle status
+    client.user.setActivity('with A BanHammer') // Set bot to idle status
 })
 
 client.on('message', message =>{
@@ -17,12 +20,67 @@ client.on('message', message =>{
     var msg_length_mod = msg.slice(1,msg_length); // Removes the first character of the string
     var exclamation_pos = msg_length_mod.indexOf('!'); //Finds the index of the first exclamation point of msg_length_mod
     var last_exclamation_pos = msg.lastIndexOf('!'); //Finds the index of the last exclamation point of msg
-    var prefix = ['Bad!']; //Prefix for this bot
-    var bot_prefix = ['!', '-','<@']; //Bot Prefixes
     var exclamation_point = 'true'; // Exclamation point tester
     var prefix_exclamation_point = []; //List for exclamation points in bot prefixes
     var mention = message.mentions.users.first(); //Searches for the first @mentions in a message 
 
+    if (msg.content.startsWith(prefix + 'ping')) { //Responds to command when prefix is used with ping
+        message.reply('Hi, Moderation_bot is online!') //Responds with "Hi, Moderation_bot is online!" when prefix with "ping" is used
+    }
+
+    if (msg.content.startsWith(prefix + 'kick ')) { //Checks if message starts with prefix and has "kick"
+        if (sender.hasPermission('ADMINISTRATOR')) {//Checks if the author has admin
+            var member = msg.mentions.members.first(); //Creates a variable that saves the first mention in message
+            member.kick().then(() => { //Kicks the first mention and then the code below activites
+                if (sender != member) { //Checks if the user kicked is not the author
+                    msg.channel.send(member.displayName + ' has successfully been kicked by ' + sender) //If successful messages the user "has been successfully kicked"
+                }
+            }).catch(e => { //If it falls the code below activates
+                msg.channel.send("An error occured") //Messages the kick has been unsuccessful and an error has occured
+            });
+        }
+    }
+
+    if (msg.content.startsWith(prefix + 'ban ')) { //Checks if message contains the prefix and "ban" to activate the rest of the code
+        if (sender.hasPermission('ADMINISTRATOR')) { //checks if the author is an admin    
+            var member = msg.mentions.members.first(); //Creates a variable for the first mention in a message if prefix + "ban" is used
+            var mc = msg.content.split(" ")[2]; 
+            member.ban(mc).then(() => { //Bans the first mention and then the rest of the code below activates
+                if (sender != member) { //Checks if the user banned is not the author
+                    msg.channel.send(member.displayName + ' has successfully been hit with the Ban Hammer by ' + sender + 'for ' + mc + ' days') //Message if the ban has been successful with the message user "has been successfully hit by the ban hammer by " author
+                }
+            }).catch(e => { //Catches the instance a ban fails
+                msg.channel.send('An error occured') //Messages if the ban has been unsuccessful with "An error occured"
+            });
+        }
+    }
+
+    if (msg.content.startsWith(prefix + 'purge')) { //checks if message starts with prefix and "purge" if so the code below activites
+        if (sender.hasPermission('ADMINISTRATOR')) { //Checks if the author is an admin
+            var mc = msg.content.split(" ")[1]; 
+            msg.channel.bulkDelete(mc); //Deletes message by the number from mc
+        }
+    }
+
+    if (msg.content.startsWith(prefix + 'setprefix')) { //Checks if the message starts with prefix and "setprefix" if so the code below will commence
+        if (sender.hasPermission('ADMINISTRATOR')) { //Checks if the authro is an admin
+            var new_prefix = msg.content.split(" ")[1];
+            prefix = new_prefix;
+        }
+    }
+
+    if (msg.content.startsWith(prefix + 'help')) { //Checks if the message starts with prefix and "help" if so code below continues
+        if (sender.hasPermission('ADMINISTRATOR')) { //Checks if the author is an admin
+            const embed = new Discord.MessageEmbed()
+                .setTitle('Commands')
+                .setAuthor('Moderation Bot')
+                .setColor('#3EEAEE')
+                .setDescription(prefix + ' help: Find the commands this bot uses \n' + prefix + ' kick "user": Kicks the "user" \n' + prefix + ' ban "user" "# of days": Bans the "user" for "# of days" \n' + prefix + ' purge "# of lines": Deletes "# of lines" from the most recent messages \n' + prefix + ' ping: Receive a message from the Moderation Bot :) \n' + prefix + ' setprefix: Changes the prefix this bot uses')
+                .setFooter('Powered By Admin Magic')
+
+                message.channel.send(embed)
+        }
+    }
 
     for (var i in bot_prefix) { //Searches through the bot_prefixes
         if (bot_prefix[i].indexOf('!') != -1) { //Locates the last exclamation point in bot_prefixes
@@ -56,8 +114,9 @@ client.on('message', message =>{
     for (var i in blacklisted) {
         if (msg.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
     }
-
-    if (mention != null) return;
+    if (foundInText == false) {
+        if (mention != null) return;
+    }
 
     if (process.env.maintain == 'on') {
         if (foundInText) {
